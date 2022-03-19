169533a9523559972b03dbb2ce93ee1e5aaec17d
k0ndrateff <kondratievegor04@yandex.ru>
2022-03-19 12:04:23 +0300

    Jump Functionality

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-var canvas = document.querySelector("canvas");
-var context = canvas.getContext("2d");
-  context.fillStyle = "red";
-  context.fillRect(10, 10, 100, 50);
+let x = 0;
+let g = -1;
+let a = 0;
+
+function setup() {
+    bg = loadImage('stroga.jpg');
+    skin = loadImage('skin1.png')
+    createCanvas(window.innerWidth, window.innerHeight); 
+}
+
+let player = {
+    x: 0,
+    y: 0, 
+    score: 0,
+};
+
+function draw() {
+    background(bg);
+    image(skin, 0, height / 1.8 - player.y, skin.width / 8, skin.height / 8);
+
+    if (mouseIsPressed && player.y == 0) {
+        a = 25;
+    }
+    player.y += a;
+    a += g;
+    if (player.y <= 0) {
+        player.y = 0;
+    }
+}
